0c5b90a6a32737923aa8f9ae7de94318307c9e0b
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-10-14 22:01:57 +0200

    Initial Commit

---
+++ WatchConnectivityCortex Watch App/WatchConnectivityCortexApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchConnectivityCortexApp.swift
+//  WatchConnectivityCortex Watch App
+//
+//  Created by Zaid Neurothrone on 2022-10-14.
+//
+
+import SwiftUI
+
+@main
+struct WatchConnectivityCortex_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WatchConnectivityCortex/WatchConnectivityCortexApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchConnectivityCortexApp.swift
+//  WatchConnectivityCortex
+//
+//  Created by Zaid Neurothrone on 2022-10-14.
+//
+
+import SwiftUI
+
+@main
+struct WatchConnectivityCortexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
